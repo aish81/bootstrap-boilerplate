31043cf9e0ed7eabe2f0349154796bebc596a9c6
Ash Henry <ashleighhenry@outlook.com>
2017-02-08 21:03:21 +0000

    init

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task('serve', ['sass'], function() {
         }
     });
 
-    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("scss/**/.scss", ['sass']);
     gulp.watch("*.html").on('change', browserSync.reload);
 });
 
